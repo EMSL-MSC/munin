589c4f3c8e8eb979de2ca729d2560968ae76e6ce
Ian Smith <ian.smith@pnnl.gov>
2015-04-07 16:16:51 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'jesse@techno-geeks.org'
 license 'Apache 2.0'
 description 'Installs and configures munin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.4.6'
+version '1.4.7'
 
-depends 'apache2', '>= 1.7'
+depends 'apache2'
 depends 'nginx', '>= 1.8'
 
 supports 'arch'
